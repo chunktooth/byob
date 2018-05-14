d65fb5b49182631b81f0370c81b551cc57f769e4
chunktooth <om.saetehn@gmail.com>
2018-05-14 17:57:36 -0600

    added all-regions valid json

--- surf-spots/clean/thailand-16.js
+++ surf-spots/clean/thailand-16.js
@@ -1,18 +1,23 @@
-[
-  { "name": "Nang Thong Beach", "latitude": 8.641730146397900, "longtitude": 98.245829343796000 },
-  { "name": "Bang Niang Rivermouth", "latitude": 8.674324150469800, "longtitude": 98.240872621536000 },
-  { "name": "Koh Lanta - Klong Dao", "latitude": 7.639146365972600, "longtitude": 99.028337001801000 },
-  { "name": "Patong Beach", "latitude":  7.903681045773700, "longtitude": 98.295879364014000 },
-  { "name": "leam sing", "latitude": 7.968032635542000, "longtitude": 98.277039527893000 },
-  { "name": "Koh Phayam", "latitude": 9.719209073116000, "longtitude": 98.393554687500000 },
-  { "name": "RAYONG - Mae RAMPHUNG BEACH", "latitude": 12.596968670368000, "longtitude": 101.408915519710000 },
-  { "name": "Cape Pakarang", "latitude": 8.738337191417900, "longtitude": 98.220520019531000 },
-  { "name": "Pansea Beach", "latitude": 7.982068356882500, "longtitude": 98.276717662811000 },
-  { "name": "Kamala", "latitude": 15.000000000000000, "longtitude": 100.000000000000000 },
-  { "name": "Karon Beach", "latitude": 7.838766734566100, "longtitude": 98.289399147034000 },
-  { "name": "Koh phangan", "latitude": 9.679614516214300, "longtitude": 100.073432922360000 },
-  { "name": "Khao lak", "latitude": 8.644328860724900, "longtitude": 98.244380950928000 },
-  { "name": "Ko Samet", "latitude": 12.568805467213000, "longtitude": 101.453804969790000 },
-  { "name": "Kata Noi", "latitude": 7.808856827113400, "longtitude": 98.297681808472000 },
-  { "name": "Kata Beach", "latitude": 7.817041343297900, "longtitude": 98.298625946045000 }
-]
+{ 
+  "region": "Thailand",
+  "center_lat": 15.8700,
+  "center_long": 100.9925,
+  "pins": [
+    { "name": "Nang Thong Beach", "latitude": 8.641730146397900, "longtitude": 98.245829343796000 },
+    { "name": "Bang Niang Rivermouth", "latitude": 8.674324150469800, "longtitude": 98.240872621536000 },
+    { "name": "Koh Lanta - Klong Dao", "latitude": 7.639146365972600, "longtitude": 99.028337001801000 },
+    { "name": "Patong Beach", "latitude":  7.903681045773700, "longtitude": 98.295879364014000 },
+    { "name": "leam sing", "latitude": 7.968032635542000, "longtitude": 98.277039527893000 },
+    { "name": "Koh Phayam", "latitude": 9.719209073116000, "longtitude": 98.393554687500000 },
+    { "name": "RAYONG - Mae RAMPHUNG BEACH", "latitude": 12.596968670368000, "longtitude": 101.408915519710000 },
+    { "name": "Cape Pakarang", "latitude": 8.738337191417900, "longtitude": 98.220520019531000 },
+    { "name": "Pansea Beach", "latitude": 7.982068356882500, "longtitude": 98.276717662811000 },
+    { "name": "Kamala", "latitude": 15.000000000000000, "longtitude": 100.000000000000000 },
+    { "name": "Karon Beach", "latitude": 7.838766734566100, "longtitude": 98.289399147034000 },
+    { "name": "Koh phangan", "latitude": 9.679614516214300, "longtitude": 100.073432922360000 },
+    { "name": "Khao lak", "latitude": 8.644328860724900, "longtitude": 98.244380950928000 },
+    { "name": "Ko Samet", "latitude": 12.568805467213000, "longtitude": 101.453804969790000 },
+    { "name": "Kata Noi", "latitude": 7.808856827113400, "longtitude": 98.297681808472000 },
+    { "name": "Kata Beach", "latitude": 7.817041343297900, "longtitude": 98.298625946045000 }
+  ]
+}
