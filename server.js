a49ce0997c6a2dcdd855448e65976724a0cdf550
Matt Walker <mttwlkr@gmail.com>
2018-05-15 16:13:41 -0600

    Add knex test seeds and setup db and create first endpoint test

--- server.js
+++ server.js
@@ -1,10 +1,31 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+const environment = process.env.NODE_ENV || 'development';
+const configuration = require('./knexfile')[environment];
+const database = require('knex')(configuration);
 
 app.set('port', process.env.PORT || 3000);
+app.use(bodyParser.json())
+
+app.locals.title = 'BYOB';
+
+app.get('/', (req, res) => {
+  res.status(200).json()
+})
+
+app.get('/api/v1/maps', (req, res) => {
+  database('maps').select()
+    .then( map => {
+      res.status(200).json(map)
+    })
+    .catch( error => {
+      res.status(404).json(error)
+    })
+})
 
 app.listen(app.get('port'), () => {
-  console.log('Server is running!');
+  console.log(`${app.locals.title} is running on Port 3000`);
 });
 
-module.exports = app; // {app, database}
+module.exports = { app, database };
--- test/routes.spec.js
+++ test/routes.spec.js
@@ -1,14 +1,58 @@
 const chai = require('chai');
 const should = chai.should();
 const chaiHttp = require('chai-http');
-const server = require('../server.js');
+const { app, database } = require('../server.js');
 
 chai.use(chaiHttp);
 
-describe('Client Routes', () => {
+describe('Testing endpoints', () => {
+  beforeEach(done => {
+    database.migrate.rollback()
+    .then(() => {
+      database.migrate.latest()
+      .then(() => {
+        database.seed.run()
+        .then(() => {
+          done();
+        });
+      });
+    });
+  });
 
-});
+  describe('GET', () => {
+    
+    it('should get all the maps', () => {
+      return chai.request(app)
+        .get('/api/v1/maps', (req, res) => {
+          res.should.have.status(200)
+          res.should.be.json;
+          res.body.should.be.an('array')
+          res.body.length.should.equal(4)
+          res.body[0].should.have.property('id')
+          res.body[0].should.have.property('region')
+          res.body[0].should.have.property('center_lat')
+          res.body[0].should.have.property('center_long')
+          res.body[0].should.have.property('created_at')
+          res.body[0].should.have.property('updated_at')
+          res.body[0].id.should.equal(1)
+          res.body[0].region.should.equal('California')
+          res.body[0].center_lat.should.equal(36.78)
+          res.body[0].center_long.should.equal(-119.42)
+        })
+    })
+  });
+  
+  describe('POST', () => {
+  
+  });
 
-describe('API Routes', () => {
+  describe('PATCH', () => {
+  
+  });
 
-});
+  describe('DELETE', () => {
+  
+  });
+
+})
+
