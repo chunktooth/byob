f18c20bb6008bbe4d09e5962d6c99388b9636adb
Matt Walker <mttwlkr@gmail.com>
2018-05-15 14:31:43 -0600

    Add travis update

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 app.set('port', process.env.PORT || 3000)
 
 app.listen(app.get('port'), () => {
-  console.log('Server is Running')
+  console.log('Server is running!')
 })
 
 module.exports = app // {app, database}
