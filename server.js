074cf16e3d19f416d4342be0324f9ace65c6b206
Matt Walker <mttwlkr@gmail.com>
2018-05-15 15:11:41 -0600

    Add and setup eslint

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const app = express();
 
-app.set('port', process.env.PORT || 3000)
+app.set('port', process.env.PORT || 3000);
 
 app.listen(app.get('port'), () => {
-  console.log('Server is running!')
-})
+  console.log('Server is running!');
+});
 
-module.exports = app // {app, database}
+module.exports = app; // {app, database}
